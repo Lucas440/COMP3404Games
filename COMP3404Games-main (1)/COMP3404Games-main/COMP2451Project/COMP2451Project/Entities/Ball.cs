b599e521690c7fca9de310c3d1ae6c97c4f809f1
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-01-24 10:42:32 +0000

    Update Ball.cs

--- COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/COMP2451Project/Entities/Ball.cs
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/COMP2451Project/Entities/Ball.cs
@@ -6,6 +6,8 @@ using System;
 
 namespace COMP2451Project
 {
+    // AUTHOR: Lucas Brennan
+    // DATE: 24/01/2022
     class Ball : PongEntity
     {
         // DECLARES a random variable called rnd
@@ -16,7 +18,7 @@ namespace COMP2451Project
         int mSpeed;
 
         /// <summary>
-        /// Is the constructor for the ball
+        /// This is the constructor for the ball
         /// </summary>
         /// <param name="ballVector"> Is used to store the starting location for the ball </param>
         public Ball(Vector2 ballVector) 
@@ -34,12 +36,12 @@ namespace COMP2451Project
 
 
         /// <summary>
-        /// Returns the entitys texture
+        /// Returns the entity's texture
         /// </summary>
-        /// <returns>Entity - The objects Texture</returns>
+        /// <returns>_Entity - The object's Texture</returns>
         public override Texture2D texture()
         {
-            //Returns Entity
+            //Returns _Entity
             return _Entity;
         }
 
@@ -48,39 +50,39 @@ namespace COMP2451Project
         /// </summary>
         public void serve() 
         {
-            //These place the ball in the center of the screen
-            //Places the ball at 450, in the x-axis
+            // These place the ball in the center of the screen
+            // Places the ball at 450, in the x-axis
             EntityLocn.X = 450;
-            //Places the ball at 450 in the y-axis
+            // Places the ball at 450 in the y-axis
             EntityLocn.Y = 450;
 
-            //DECLARES a float called loaction
+            // DECLARES a float called Rotation
             float Rotation;
-            // Randomizes a number which detemains the angle of travel
+            // Randomizes a number which determines the angle of travel
             Rotation = (float)(Math.PI / 2 + (rnd.NextDouble() * (Math.PI / 1.5f) - Math.PI / 3));
 
-            //Uses the sin of roatation to determain the x velocity
+            // Uses the sin of rotation to determine the x velocity
             Velocity.X = (float)Math.Sin(Rotation);
-            // uses the cos of roatation to determain the y velocity
+            // uses the cos of rotation to determine the y velocity
             Velocity.Y = (float)Math.Cos(Rotation);
 
-            // randoms a number between 0 and 1 if it is 1 this is true
-            // gives a 50 50 chance to start the ball going left or right
+            // Randomly generates a number between 0 and 1. If it is 1, then this is true:
             if (rnd.Next(0,2) == 1) 
             {
+                // The ball will move to the left
                 Velocity.X *= -1;
             }
-            // changes to total velocity to go at at least 5
+            // Multiplies the ball's velocity by the set speed
             Velocity *= mSpeed;
 
         }
         /// <summary>
-        /// Used to Set Entity
+        /// Used to Set _Entity
         /// </summary>
         /// <param name="ballTexture">Passes the balls texture</param>
         public override void Content(Texture2D ballTexture)
         {
-            //INITALIZES Entity in the parent class
+            //INITALIZES _Entity in the parent class
             _Entity = ballTexture;
         }
         /// <summary>
@@ -88,9 +90,9 @@ namespace COMP2451Project
         /// </summary>
         public override void update()
         {
-            // Adds the Current X speed onto the x loction
+            // Moves the ball along the x-axis by the ball's x velocity
             EntityLocn.X += Velocity.X;
-            // adds the current y speed onto the y location
+            // Moves the ball along the y-axis by the ball's y velocity
             EntityLocn.Y += Velocity.Y;
             // calls update in the parent class
             base.update();
@@ -106,7 +108,7 @@ namespace COMP2451Project
             // if the ball's y position is less than 0 or greater than the screens height then this is true
             if (EntityLocn.Y < 0 || EntityLocn.Y > Height - 45) 
             {
-                // times the Y velocity by -1;
+                // multiplies the Y velocity by -1, reversing the vertical direction of the ball
                 Velocity.Y *= -1;
             }
             // if the ball's x position is less than 0 or greater than the screens width then this is true
@@ -122,7 +124,10 @@ namespace COMP2451Project
         /// </summary>
         public override void colision() 
         {
+            // Reverse the horizontal direction of the ball
             Velocity.X *= -1;
+            
+            // Update the ball's location
             EntityLocn.X += Velocity.X;
         }
 
@@ -132,7 +137,7 @@ namespace COMP2451Project
         /// <param name="paddleDirection">Passes the paddles velocity</param>
         public void Colision(Vector2 paddleDirection)
         {
-            // times the x velocity by -1;
+            // multiply the x velocity by -1;
             Velocity.X *= -1;
 
             // if the paddle's y direction is greater than 0 this is true
