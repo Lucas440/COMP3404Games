6f49fcddd4b083bc45fd1a7236c6559b45ddcc06
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-01-24 12:20:04 +0000

    Ball added to Facade pattern

--- COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/COMP2451Project/Behaviours/BallBehaviour.cs
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/COMP2451Project/Behaviours/BallBehaviour.cs
@@ -4,19 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/// Author Lucas Brennan
-/// 
-/// DATE 22/01/22
-/// </summary>
+
 namespace COMP2451Project.Behaviours
 {
-    class BallBehaviour
+    /// <summary>
+    /// AUTHOR: Flynn Osborne & Lucas Brennan
+    /// DATE: 24/01/2022
+    /// </summary>
+    class BallBehaviour: PongBehaviour, IUpdateListener
     {
-        /// <summary>
-        /// A method that is called when the sender is updated
-        /// </summary>
-        /// <param name="sender">The object that sent the event</param>
-        /// <param name="UpdateEventArgs">The event argument</param>
-        public void OnUpdate(Object sender, EventArgs UpdateEventArgs) { }
+        public override Vector2 Velocity { get => _velocity; }
+
+        public BallBehaviour()
+        {
+            _velocity = new Vector2();
+        }
+
+        public override void OnUpdate(Object sender, UpdateEventArgs UpdateEventArgs) 
+        {    
+            if (UpdateEventArgs.ActiveBehaviour.ToLower() == "serve")
+            {
+                // DECLARES a float called Rotation
+                float Rotation;
+
+                // DECLARES a random variable called rnd
+                Random rnd = new Random();
+
+                // Randomizes a number which determines the angle of travel
+                Rotation = (float)(Math.PI / 2 + (rnd.NextDouble() * (Math.PI / 1.5f) - Math.PI / 3));
+
+                // Uses the sin of rotation to determine the x velocity
+                _velocity.X = (float)Math.Sin(Rotation);
+                // uses the cos of rotation to determine the y velocity
+                _velocity.Y = (float)Math.Cos(Rotation);
+
+                // Randomly generates a number between 0 and 1. If it is 1, then this is true:
+                if (rnd.Next(0, 2) == 1)
+                {
+                    // The ball will move to the left
+                    _velocity.X *= -1;
+                }
+            }
+        
+        
+        }
     }
 }
