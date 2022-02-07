4fdfc16ea13c78406c0e902a28461e15d0c9625f
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-02-07 11:57:35 +0000

    Add files via upload

---
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/COMP2451Project/States/IState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2451Project.States
+{
+    // AUTHOR: Flynn Osborne
+    // DATE: 31/01/2022
+
+    public interface IState
+    {
+        /// <summary>
+        /// The method used to update the entity's state
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        /// The method triggered when the entity collides
+        /// </summary>
+        void Collide();
+    }
+}
---
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/COMP2451Project/States/State.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using COMP2451Project.Behaviours;
+
+namespace COMP2451Project.States
+{
+    // AUTHOR: Flynn Osborne
+    // DATE: 07/02/2022
+
+    public abstract class State : IState
+    {
+        // A dictionary to hold any commands
+        protected IDictionary<string, ICommand> _trigger;
+
+        // A property to set an return a state's linked entity
+        public IEntity _entity { set; get; }
+
+        // A variable to hold the entity's linked behavior
+        public IBehaviour _behaviour;
+
+        /// <summary>
+        /// The method used to update the entity's state
+        /// </summary>
+        public virtual void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// The method triggered when the entity has a collision
+        /// </summary>
+        public virtual void Collide()
+        {
+
+        }
+    }
+}
