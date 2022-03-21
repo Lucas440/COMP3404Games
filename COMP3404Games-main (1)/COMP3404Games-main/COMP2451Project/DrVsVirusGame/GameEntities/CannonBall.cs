3555dbbc7cf515314bc4cba89dfc7d0096ca4325
Lucas Brennan <brel1_19@uni.worc.ac.uk>
2022-03-21 09:46:13 +0000

    CannonBall CleanUp

--- COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/DrVsVirusGame/GameEntities/CannonBall.cs
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/DrVsVirusGame/GameEntities/CannonBall.cs
@@ -17,75 +17,99 @@ namespace DrVsVirusGame.GameEntities
     public class CannonBall : DrVsVirusEntity
     {
         //DECLARE A Vector2 called _moveToo
-        Vector2 _moveToo;
+        private Vector2 _target;
         //DECLARE a Bool called _moving
-        bool _moving;
+        private bool _isMoving;
 
-        bool xReached;
-        bool yReached;
+        //DECLARE two bools _xReached and _yReached
+        private bool _xReached;
+        private bool _yReached;
 
         /// <summary>
         /// The Default Constructor
         /// </summary>
         public CannonBall() 
         {
+            //Sets the location to -1 -1
             _entityLocn.X = -1;
             _entityLocn.Y = -1;
-            _moving = false;
+            //Sets _isMoving to false
+            _isMoving = false;
         }
-
-        public void StartMoving(Vector2 pMoveToo) 
+        /// <summary>
+        /// A method used to start the cannonball moving
+        /// </summary>
+        /// <param name="pTarget">The target of the cannonball</param>
+        public void StartMoving(Vector2 pTarget) 
         {
-            _moveToo = pMoveToo;
-            _moving = true;
-            xReached = false;
-            yReached = false;
+            //Sets the _target to pTarget
+            _target = pTarget;
+            //sets _isMoving to true
+            _isMoving = true;
+            //Sets xReached and yReached flags to false
+            _xReached = false;
+            _yReached = false;
         }
         /// <summary>
         /// A method used to update the object
         /// </summary>
         public override void update()
         {
+            //Calls update in the base class
             base.update();
-
-            if (_moving == true) 
+            //If the moving flag is true this is true
+            if (_isMoving == true) 
             {
-
-                if (_entityLocn.X <= _moveToo.X && xReached == false) 
+                //If the entites X position is less than the X position it needs to move to
+                // And xReached flag is false, This is true
+                if (_entityLocn.X <= _target.X && _xReached == false) 
                 {
+                    //Increments the x position
                     _entityLocn.X++;
                 }
-                else if (_entityLocn.X > _moveToo.X && xReached == false) 
+                //If the entites X position is more than the X position it needs to move to
+                // And xReached flag is false, This is true
+                else if (_entityLocn.X > _target.X && _xReached == false) 
                 {
+                    //Reduces the X position
                     _entityLocn.X--;
                 }
-
-                if (_entityLocn.X  >= _moveToo.X - 10 && _entityLocn.X <= _moveToo.X + 10) 
+                //If the x position is Greater than the target position -10
+                //And the x position is less than the target position +10
+                //this is true
+                if (_entityLocn.X  >= _target.X - 10 && _entityLocn.X <= _target.X + 10) 
                 {
-                    xReached = true;
+                    //Sets the xReached flag to true
+                    _xReached = true;
                 }
-
-                if (_entityLocn.Y <= _moveToo.Y) 
+                //If the entites y position is less than the y position its target
+                // And yReached flag is false, This is true
+                if (_entityLocn.Y <= _target.Y && _yReached == false) 
                 {
+                    //Increments the Y position
                     _entityLocn.Y++;
                 }
-                else if (_entityLocn.Y > _moveToo.Y)
+                //If the entites y position is more than the y position its target
+                // And yReached flag is false, This is true
+                else if (_entityLocn.Y > _target.Y && _yReached == false)
                 {
+                    //Reduces the yPosition
                     _entityLocn.Y--;
                 }
-                else 
+                //If the y position is Greater than the target position -10
+                //And the y position is less than the target position +10
+                //this is true
+                if (_entityLocn.Y >= _target.Y - 10 && _entityLocn.Y <= _target.Y + 10)
                 {
-                    yReached = true;
+                    //Sets the yReached flag to true
+                    _yReached = true;
                 }
-
-                if (_entityLocn.Y >= _moveToo.Y - 10 && _entityLocn.Y <= _moveToo.Y + 10)
+                //If both flags are true this is true
+                if (_xReached && _yReached) 
                 {
-                    yReached = true;
-                }
-
-                if (xReached && yReached) 
-                {
-                    _moving = false;
+                    //Sets the moving flag to false
+                    _isMoving = false;
+                    //Sets the entity back to 0 0 
                     _entityLocn.Y = 0;
                     _entityLocn.X = 0;
                 }
