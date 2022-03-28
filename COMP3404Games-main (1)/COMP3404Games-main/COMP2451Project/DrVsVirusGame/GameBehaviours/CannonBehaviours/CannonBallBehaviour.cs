cf729106c19ddd086fee94371030036d31bbedf6
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-03-28 11:10:50 +0100

    Comments

--- COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/DrVsVirusGame/GameBehaviours/CannonBehaviours/CannonBallBehaviour.cs
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/DrVsVirusGame/GameBehaviours/CannonBehaviours/CannonBallBehaviour.cs
@@ -8,38 +8,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /// <summary>
-/// Author Lucas Brennan
+/// AUTHOR: Lucas Brennan
 /// 
-/// Date 21/03/22
+/// DATE: 21/03/22
 /// </summary>
 namespace DrVsVirusGame.GameBehaviours.CannonBehaviours
 {   
     /// <summary>
-    /// A Class that represents the Cannon Balls Behaviour 
+    /// A Class that represents the Cannonball's Behaviour 
     /// </summary>
     public class CannonBallBehaviour : DrVsVirusBehaviour
     {
         /// <summary>
-        /// A Property to determain if the CannonBall is moving
+        /// A Property to determine if the CannonBall is moving
         /// </summary>
         public bool IsMoving { get; set; }
 
-        //DECLARE two floats called _xSpeed and _ySpeed
+        // DECLARE two floats called _xSpeed and _ySpeed
         private float _xSpeed;
         private float _ySpeed;
 
-        //DECLARE an int called _time
+        // DECLARE an int called _time
         private int _time;
 
         /// <summary>
-        /// the default constructor
+        /// The default constructor
         /// </summary>
         public CannonBallBehaviour() 
         {
-            //Sets _xSpeed and _ySpeed back to 0
+            // SETS _xSpeed and _ySpeed back to 0
             _ySpeed = 0;
             _xSpeed = 0;
-            //Sets time to 60 frames of 1 second
+
+            // SETS the time to 60 frames per second
             _time = 180;
         }
 
@@ -50,89 +51,89 @@ namespace DrVsVirusGame.GameBehaviours.CannonBehaviours
         /// <param name="pCannonBall"></param>
         private void CalculateSpeed(CannonBall pCannonBall) 
         {
-            //Calculates the speed by using the distance from the target divided by the time
+            // CALCULATE the speed by using the distance from the target divided by the time
             _xSpeed = (pCannonBall.Target.X - _location.X) / _time;
             _ySpeed = (pCannonBall.Target.Y - _location.Y) / _time;
         }
 
         /// <summary>
-        /// A Method that responds to an update
+        /// A method that responds to an update
         /// </summary>
         /// <param name="sender">The object that is being updated</param>
         /// <param name="UpdateEventArgs">The event argument</param>
         public override void OnUpdate(object sender, UpdateEventArgs UpdateEventArgs)
         {
-            //DECLARES xReached and yReached flags and sets them to false
+            // DECLARES xReached and yReached flags and sets them to false
             bool _xReached = false;
             bool _yReached = false;
 
-            //Sets _location to the sender position
+            // SETS _location to the sender position
             _location = ((IEntity)sender).Position;
 
-            //If xSpeed and _ySpeed are 0 this is true
+            // IF xSpeed and _ySpeed are 0:
             if (_xSpeed == 0 && _ySpeed == 0) 
             {
-                //Calls CalculateSpeed
+                // CALL CalculateSpeed
                 CalculateSpeed((CannonBall)sender);
             }
 
 
-            //If the entites X position is less than the X position it needs to move to
-            // And xReached flag is false, This is true
+            // IF the entity's X position is less than the X position it needs to move to
+            // and xReached flag is false:
             if ( (_location.X  <= ((CannonBall)sender).Target.X && _xReached == false))
             {
-                //Increments the x position
+                // INCREMENTS the x position
                 _location.X += _xSpeed;
             }
-            //If the entites X position is more than the X position it needs to move to
-            // And xReached flag is false, This is true
+
+            // IF the entity's X position is more than the X position it needs to move to
+            // and xReached flag is false:
             else if (_location.X > ((CannonBall)sender).Target.X && _xReached == false)
             {
-                //Reduces the X position
+                // REDUCE the X position
                 _location.X-= _xSpeed;
             }
-            //If the x position is Greater than the target position -10
-            //And the x position is less than the target position +10
-            //this is true
+
+            // IF the X position is Greater than the target position -10
+            // and the X position is less than the target position +10:
             if (_location.X >= ((CannonBall)sender).Target.X - 10 && _location.X <= ((CannonBall)sender).Target.X + 10)
             {
-                //Sets the xReached flag to true
+                // SET the xReached flag to true
                 _xReached = true;
             }
-            //If the entites y position is less than the y position its target
-            // And yReached flag is false, This is true
+            // IF the entity's Y position is less than the Y position its target
+            // and yReached flag is false:
             if (_location.Y <= ((CannonBall)sender).Target.Y && _yReached == false)
             {
-                //Increments the Y position
+                // INCREASE the Y position
                 _location.Y+= _ySpeed;
             }
-            //If the entites y position is more than the y position its target
-            // And yReached flag is false, This is true
+            // IF the entity's Y position is more than the Y position its target
+            // and yReached flag is false:
             else if (_location.Y > ((CannonBall)sender).Target.Y && _yReached == false)
             {
-                //Reduces the yPosition
+                // REDUCE the yPosition
                 _location.Y-= _ySpeed;
             }
-            //If the y position is Greater than the target position -10
-            //And the y position is less than the target position +10
-            //this is true
+            // IF the y position is Greater than the target position -10
+            // and the y position is less than the target position +10:
             if (_location.Y >= ((CannonBall)sender).Target.Y - 10 && _location.Y <= ((CannonBall)sender).Target.Y + 10)
             {
-                //Sets the yReached flag to true
+                // SETS the yReached flag to true
                 _yReached = true;
             }
-            //If both flags are true this is true
+            // IF both flags are true:
             if (_xReached && _yReached)
             {
-                //Sets the entity back to 0 0 
+                // SETS the entity back to 0,0 
                 _location.Y = 0;
                 _location.X = 0;
 
-                //Sets _xSpeed and _ySpeed back to 0
+                // SET _xSpeed and _ySpeed back to 0
                 _ySpeed = 0;
                 _xSpeed = 0;
 
-                //Sets IsMoving to false
+                // SET IsMoving to false
                 IsMoving = false;
             }
         }
