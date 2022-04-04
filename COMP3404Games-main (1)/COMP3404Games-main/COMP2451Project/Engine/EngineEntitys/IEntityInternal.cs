da59c5aaa2073f413b5c6315436b884a1256368f
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-04-04 11:40:30 +0100

    Comments

--- COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/Engine/EngineEntitys/IEntityInternal.cs
+++ COMP3404Games-main (1)/COMP3404Games-main/COMP2451Project/Engine/EngineEntitys/IEntityInternal.cs
@@ -1,22 +1,22 @@
 ﻿using Engine.Command;
 /// <summary>
-/// Author Lucas Brennan
-/// 
-/// Date 22/01/22
+/// AUTHOR: Lucas Brennan
+/// DATE: 22/01/22
 /// </summary>
 namespace Engine.EngineEntitys
 {
     /// <summary>
-    /// An interface that represents the internal Entity
+    /// An interface that represents the internal entity
     /// </summary>
     public interface IEntityInternal
     {
         /// <summary>
-        /// A Propertiy used to terminate the entity
+        /// A property used to terminate the entity
         /// </summary>
         ICommand TerminateMe { get; set; }
+        
         /// <summary>
-        /// A Propertity used to remove the entity
+        /// A property used to remove the entity
         /// </summary>
         ICommand RemoveMe { get; set; }
     }
